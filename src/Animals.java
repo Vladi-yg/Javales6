5bac262af566b922823c65f6dc26c2115016e486
Vladi-yg <vladzeynalov@icloud.com>
2020-05-25 11:33:33 +0300

    Initial commit

---
+++ src/Animals.java
@@ -0,0 +1,10 @@
+public class Animals {
+    protected String color;
+    protected String name;
+    private int age;
+    public Animals(String color, String name) {
+        this.color = color;
+        this.name = name;
+        this.age = age;
+    }
+}
